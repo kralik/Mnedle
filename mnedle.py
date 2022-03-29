ceeebe41613942e20261622f802e1771d76ef36a
Kralik <vodraska@estetica.cz>
2022-03-29 23:06:44 +0200

    2 pridani knihovny pick a jeji pouziti

--- mnedle.py
+++ mnedle.py
@@ -3,7 +3,7 @@
 # Mnedle: CZ Wordle
 
 import sys
-from os import system, name
+from pick import pick   # rozhodovaci menu (https://github.com/wong2/pick), pip install pick
 
 # nacteni vlastni knihovny 5 mistnich ceskych slov
 import slova5 # promenna words5
@@ -12,70 +12,39 @@ import slova5 # promenna words5
 score = 0
 
 # menu moznosti
-menu_options = {
-    1: 'Pokračovat',
-    2: 'Konec'
-}
+options = ['Začni hru', 'Konec']
 
 def print_logo():
-    print('                                                     ')
-    print('███╗   ███╗███╗   ██╗███████╗██████╗ ██╗     ███████╗')
-    print('████╗ ████║████╗  ██║██╔════╝██╔══██╗██║     ██╔════╝')
-    print('██╔████╔██║██╔██╗ ██║█████╗  ██║  ██║██║     █████╗  ')
-    print('██║╚██╔╝██║██║╚██╗██║██╔══╝  ██║  ██║██║     ██╔══╝  ')
-    print('██║ ╚═╝ ██║██║ ╚████║███████╗██████╔╝███████╗███████╗')
-    print('╚═╝     ╚═╝╚═╝  ╚═══╝╚══════╝╚═════╝ ╚══════╝╚══════╝')
-    print('                                                     ')
+    logo = '                                                     \n'
+    logo +='███╗   ███╗███╗   ██╗███████╗██████╗ ██╗     ███████╗\n'
+    logo +='████╗ ████║████╗  ██║██╔════╝██╔══██╗██║     ██╔════╝\n'
+    logo +='██╔████╔██║██╔██╗ ██║█████╗  ██║  ██║██║     █████╗  \n'
+    logo +='██║╚██╔╝██║██║╚██╗██║██╔══╝  ██║  ██║██║     ██╔══╝  \n'
+    logo +='██║ ╚═╝ ██║██║ ╚████║███████╗██████╔╝███████╗███████╗\n'
+    logo +='╚═╝     ╚═╝╚═╝  ╚═══╝╚══════╝╚═════╝ ╚══════╝╚══════╝\n'
+    return logo
 
-def print_hr():
-    print('----------------')
-
-def clear():
-    # win
-    if name == 'nt':
-        _ = system('cls')
-    # mac, linux (posix)
-    else:
-        _ = system('clear')
-
-def print_menu():
-    for key in menu_options.keys():
-        print (key, '=', menu_options[key])
+title = print_logo()
 
 # sklonovani slova bod, program match zde nepouzivam kvuli kompatibilite se starsimi verzemi pythonu
 def inflection(score):
-    infl = ' bod'
     switcher = {
         1: '',
         2: 'y',
         3: 'y',
         4: 'y'
     }
-    return infl + str(switcher.get(abs(score), 'ů'))
+    return 'bod' + str(switcher.get(abs(score), 'ů'))
 
 def start():
     global score
-    print('Celkové skóre: ' + str(score) + inflection(4))
-
-
+    print('Celkové skóre: ' + str(score) + ' ' + inflection(score))
 
 # Main
 if __name__ == "__main__":
 
-    while(True):
-        clear()
-        print_logo()
-        print_menu()
-        print_hr()
-        option = ''
-        try:
-            option = int(input('Napiš možnost: '))
-        except:
-            print('Musí být pouze číslo ...')
-        if option == 1:
-            start()
-        elif option == 2:
-            exit()
-        else:
-            print('Pouze čísla 1 pro pokračování a 2 pro konec')
-        print_hr()
+    option, index = pick(options, title, indicator = '->')
+    if (index == 0):
+        start()
+    if (index == 1):
+        exit()
