b27e3cfad18e20c30a025d970b4594a23d4f3bbc
Kralik <vodraska@estetica.cz>
2022-04-01 00:18:13 +0200

    3 pouziti knihovny numpy pro hraci pole

--- mnedle.py
+++ mnedle.py
@@ -3,7 +3,9 @@
 # Mnedle: CZ Wordle
 
 import sys, random
-from pick import pick   # rozhodovaci menu (https://github.com/wong2/pick), pip install pick
+from pick import pick                   # rozhodovaci menu (https://github.com/wong2/pick), pip install pick
+import numpy as np                      # knihovna s 2d polem, pip install numpy
+from termcolor import colored, cprint   # ANSII Color formatting for output in terminal, pip install termcolor
 
 # nacteni vlastni knihovny 5 mistnich ceskych slov
 import slova5 # promenna words5
@@ -26,39 +28,53 @@ def print_logo():
 options = ['Začni hru', 'Konec']
 title = print_logo()
 
+# barvicky
+colors = {
+    'default': 'blue',
+    'bad_input': 'red',
+    'actual_row': 'magenta',
+    'with_pos': 'green',
+    'without_pos': 'yellow'
+}
+
 # 2D hraci pole 5x5 (resp. 6x5 s popiskem), pet pokusu petimistnych slov
 class GameField(object):
     
     matrix = []
     defaultChar = '⏺'
-    badChar = ' ᳵ '
+    badChar = 'ᳵ'
     gapChar = ' '
     colon = ':'
+    word = ''
 
     def __init__(self, w, h):
 
         self.w = int(w)
         self.h = int(h)
-        
-        for y in range(1, self.h):
-            self.matrix.append([])
-            for x in range(0, self.w):
-                if (x == 0):
-                    self.matrix[x].append(str(self.h-(y-1)) + self.colon + self.gapChar)
-                else:
-                    self.matrix[x].append(self.defaultChar)
+
+        self.word = random.choice(list(slova5.words5.items()))[1]
+
+        self.matrix = np.array([[self.defaultChar]*self.w]*self.h)
+
+        for i in range(0, len(self.matrix)):
+            self.matrix[i,0] = str(self.h-((i+1)-1))
 
     def listingGameField(self):
         
         view = ''
         
-        for y in range(self.h):
+        for r in range(0, len(self.matrix)):
             row = ''
-            for x in range(self.w):
-                row += self.matrix[x][y]
+            first = True
+            for item in self.matrix[r]:
+                if (first):
+                    row += item + self.colon
+                    first = False
+                else:
+                    row += self.gapChar + item + self.gapChar
             view += row + '\n'
         
-        return view
+        return colored(view,colors['default'])
 
 
 # sklonovani slova bod
@@ -73,9 +89,9 @@ def inflection(score):
 
 def start():
     global score
-    print('Celkové skóre: ' + str(score) + ' ' + inflection(score))
-    game = GameField(5,5)
-    print(game.listingGameField())
+    print('Celkové skóre: ' + str(score) + ' ' + inflection(score) + '\n')
+    gf = GameField(6,5)
+    print(gf.listingGameField())
 
 # Main
 if __name__ == "__main__":
